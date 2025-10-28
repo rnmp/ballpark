ff405b416d26632cca1ad7ebbb72562d0fa2b113
Rolando Murillo <rolandomurillo@hey.com>
2025-10-27 23:15:25 -0400

    🇵🇪

--- money.js
+++ money.js
@@ -6,23 +6,33 @@ export function sanitizeMoneyInput(str) {
 
 export function money(int, mode) {
   const currency = getCurrency()
+  let language = 'en-US'
+  if (currency === 'PEN') {
+    language = 'es-PE'
+  }
 
-  const account = new Intl.NumberFormat("en-US", {
+  const account = new Intl.NumberFormat(language, {
     style: "currency",
     currency,
     maximumFractionDigits: 0,
   });
 
-  const netWorth = new Intl.NumberFormat("en-US", {
+  const netWorth = new Intl.NumberFormat(language, {
     style: "currency",
     currency,
     maximumSignificantDigits: 3,
     notation: "compact",
   })
 
+  let value = account.format(int) 
+
   if (mode === 'total') {
-    return netWorth.format(int)
+    value = netWorth.format(int)
   }
 
-  return account.format(int)
+  if (currency === 'PEN') {
+    value = value.replace('S/', 'S/.')
+  }
+
+  return value 
 }
