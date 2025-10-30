fd72a9a3b0295a5ccbf3d7ebb250fc512188538b
Rolando Murillo <rolandomurillo@hey.com>
2025-10-30 18:22:33 -0400

    Fix input

--- components.js
+++ components.js
@@ -2,41 +2,17 @@ import { make } from './dom.js'
 
 export function editableText(node, text, save) {
   node.classList.toggle('editable-text', true)
-  node.textContent = text
-
-  let editing = false
 
   const input = make('input')
   input.value = text
-
-  const complete = () => {
-    if (!editing) {
-      return
-    }
-
-    editing = false 
-    node.innerHTML = input.value
-
+  input.addEventListener('blur', () => {
     if (input.value === text) {
-      node.innerHTML = text
       return 
     }
     save(input.value)
-  }
-  
-  input.addEventListener('blur', complete)
-
-  document.addEventListener('click', complete)
-
-  node.addEventListener('click', (event) => {
-    event.stopPropagation()
-
-    editing = true
-
-    node.innerHTML = ''
-    node.append(input)
-    input.focus()
   })
+
+  node.append(input)
 
   return node
 }
